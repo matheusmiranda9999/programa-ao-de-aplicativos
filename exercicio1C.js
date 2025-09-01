8bdf725379f8edf771b2b8f4cc524b0544ed8145
matheusmiranda9999 <mirandamatheus482@gmail.com>
2025-09-01 09:08:47 -0300

    Create exercicio1C.js

---
+++ exercicio1C.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let numeroA = Number(prompt("Digite um número: "));
+let numeroB = Number(prompt("Digite um número: "));
+
+console.log("A soma dos números é: ", numeroA + numeroB);
