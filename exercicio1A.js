72b88329336a166c98976578b4d583edba693b3f
matheusmiranda9999 <mirandamatheus482@gmail.com>
2025-09-01 09:06:33 -0300

    Create exercicio1A.js

---
+++ exercicio1A.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let nome = prompt("Digite seu nome: ");
+let idade = prompt("Digite sua idade: ");
+let altura = prompt("Digite sua altura: ");
+console.log("Meu nome é", nome, "tenho", idade, "anos e", altura, "de altura.");p
